54a21d921bbcfe903d61f8a2540cc7bbc4ab4200
devZevitor <vict.95977@gmail.com>
2026-07-31 21:11:48 -0300

    set up architecture folder

---
+++ src/paginas/SubjectDetailsPage/useSubjectDetails.tsx
@@ -0,0 +1,7 @@
+export const HomePage = () => {
+    return (
+        <div className="w-screen h-screen bg-[#171819] p-4">
+            <h2 className="text-white font-bold"> Pagina detalhes da materia!</h2>
+        </div>
+    )
+}
